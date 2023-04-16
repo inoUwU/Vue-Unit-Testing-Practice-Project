7da1efca342f00f5955eaa9d8da38cfc56a607ec
ino <87593133+inoUwU@users.noreply.github.com>
2023-04-16 22:50:25 +0900

    eslintとprettierの導入+フォーマット

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,16 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
 // primevue
-import PrimeVue from "primevue/config";
+import PrimeVue from 'primevue/config';
 //theme
-import "primevue/resources/themes/lara-light-indigo/theme.css";
+import 'primevue/resources/themes/lara-light-indigo/theme.css';
 //core
-import "primevue/resources/primevue.min.css";
+import 'primevue/resources/primevue.min.css';
 //icons
-import "primeicons/primeicons.css";
+import 'primeicons/primeicons.css';
 
 const app = createApp(App);
 app.use(PrimeVue);
-app.mount("#app");
+app.mount('#app');
